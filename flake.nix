e1a0de0ed962224b5ff1e6a62d4b82a8cddae5b9
Oxke <osea.fracchia@iusspavia.it>
2025-09-28 01:45:34 +0200

    fix nixvim

--- flake.nix
+++ flake.nix
@@ -10,16 +10,20 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, nixos-hardware }: {
+  outputs = { self, nixpkgs, ... } @ inputs: {
     nixosConfigurations = {
       nixosea = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
-          home-manager.nixosModules.home-manager
-          nixos-hardware.nixosModules.framework-13-7040-amd
+          inputs.home-manager.nixosModules.home-manager
+          inputs.nixos-hardware.nixosModules.framework-13-7040-amd
         ];
       };
     };
