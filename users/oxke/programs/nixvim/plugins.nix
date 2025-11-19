3024b5ed6be1eeb06800644c8a3af16e86fb6db3
Oxke <osea.fracchia@iusspavia.it>
2025-11-19 17:09:43 +0100

    remove nvf

--- users/oxke/programs/nixvim/plugins.nix
+++ users/oxke/programs/nixvim/plugins.nix
@@ -12,18 +12,77 @@
           nixd.enable = true;
           ts_ls.enable = true;
           lua_ls.enable = true;
-          ltex.enable = true;
+          ltex = {
+            enable = true;
+            settings.language = "it";
+            settings.additionalLanguages = [ "en-US" "fr" ];
+          };
         };
       };
       cmp = {
         enable = true;
         autoEnableSources = true;
-        settings.sources = [
-          { name = "nvim_lsp"; }
-          { name = "path"; }
-          { name = "buffer"; }
-        ];
+        settings = {
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "path"; }
+            { name = "buffer"; }
+            { name = "ultisnips"; }
+            { name = "luasnip"; }
+            { name = "vimtex"; }
+          ];
+          mapping = { __raw = ''{
+              ['<cr>'] = cmp.mapping.confirm {
+                behavior = cmp.ConfirmBehavior.Insert,
+                select = true,
+              },
+
+              ['<A-l>'] = function(fallback)
+                -- UltiSnips: expand or jump forward if possible
+                if vim.fn["UltiSnips#CanExpandSnippet"]() == 1 then
+                  return vim.fn["UltiSnips#ExpandSnippet"]()
+                elseif vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
+                  return vim.fn["UltiSnips#JumpForwards"]()
+                end
+
+                -- Otherwise confirm completion
+                cmp.mapping.confirm {
+                  behavior = cmp.ConfirmBehavior.Insert,
+                  select = true,
+                }(fallback)
+              end,
+
+              ['<Tab>'] = function(fallback)
+                if not cmp.select_next_item() then
+                  if vim.bo.buftype ~= 'prompt' and has_words_before() then
+                    cmp.complete()
+                  else
+                    fallback()
+                  end
+                end
+              end,
+
+              ['<S-Tab>'] = function(fallback)
+                if not cmp.select_prev_item() then
+                  if vim.bo.buftype ~= 'prompt' and has_words_before() then
+                    cmp.complete()
+                  else
+                    fallback()
+                  end
+                end
+              end,
+          }'';};
+          snippet.expand = ''
+            function(args)
+              -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+              require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+              -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+              vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+            end
+          '';
+        };
       };
+      luasnip.enable = true;
       friendly-snippets.enable = true;
       tiny-inline-diagnostic.enable = true;
       telescope = {
@@ -35,7 +94,7 @@
               desc = "Telescope Git Files";
             };
           };
-          "<leader>fg" = {
+          "<leader>fd" = {
             action = "live_grep";
             options = {
               desc = "Telescope Live Grep";
